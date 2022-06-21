c59ff16039b3b0d16720adbd74b0433a445d8196
oathhh <y.yaowasrisuwan@outlook.com>
2022-06-21 13:37:51 +0700

    packge

---
+++ oop1/oop2/cusorder/item.py
@@ -0,0 +1,9 @@
+class Item:
+    def init(self) -> None:
+        self.shipping = ''
+        self.description = ''
+
+    def get_price(self):
+        pass
+    def get_weight(self):
+        pass
